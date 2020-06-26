002f454d14cd32c298fa1ba237ab89e8d9729e5a
Cooper Saye <coopersaye@gmail.com>
2020-06-26 13:03:07 -0700

    Implement currency counter.

---
+++ Assets/Counter.cs
@@ -0,0 +1,43 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class Counter : MonoBehaviour
+{
+    public int count;
+    [Range(0.01f, 1)] public float speed;
+
+    public TextMeshProUGUI counter;
+    public TextMeshProUGUI plusCounter;
+
+    private int currentCount;
+    private int currentPlus;
+
+    private bool startIncrease;
+    
+    private float lastCallTime;
+
+    private void Update()
+    {
+        if (count > currentCount)
+        {
+            startIncrease = true;
+            plusCounter.enabled = true;
+        }
+        else
+        {
+            startIncrease = false;
+            plusCounter.enabled = false;
+            currentPlus = 0;
+        }
+
+        if (startIncrease && Time.time - lastCallTime >= speed)
+        {
+            lastCallTime = Time.time;
+            currentCount++;;
+            currentPlus++;
+        }
+
+        counter.text = currentCount.ToString();
+        plusCounter.text = "+" + currentPlus.ToString();
+    }
+}
